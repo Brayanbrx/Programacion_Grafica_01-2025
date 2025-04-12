1ec2e6a4d299d05cb31813d43d9985846708dccc
Brayan Edgar Blanco Camacho <brayanbrx1@gmail.com>
2025-04-11 23:54:16 -0400

    Eliminados

--- OpenTK_Practico_3/OpenTK/obj/Debug/net8.0/OpenTK.AssemblyInfo.cs
+++ OpenTK_Practico_3/OpenTK/obj/Debug/net8.0/OpenTK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenTK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e7af2a809812bdd09619480c50e73f6eabdeb90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f84409ae38bac31d30adbcf7e0b8b6bb6f448e3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenTK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenTK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
